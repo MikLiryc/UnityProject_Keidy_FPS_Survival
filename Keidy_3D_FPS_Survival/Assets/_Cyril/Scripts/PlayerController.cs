6e1b7ee2d54c971fbb4a53fb4e497639eb2ba7b2
Cyril Kim <jay.kim1994@gmail.com>
2020-11-16 17:25:14 +0900

    Created new project Added provided assets (Images, 3DModels etc.)

---
+++ Keidy_3D_FPS_Survival/Assets/_Cyril/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private float lookSensitivity;
+
+    [SerializeField]
+    private float cameraRotationLimit;
+    private float currentCameraRotationX = 0f;
+
+    [SerializeField]
+    private Camera playerCamera;
+    private Rigidbody myRigid;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        myRigid = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        cameraRotation();
+        characterRotation();
+    }
+
+    private void characterRotation()
+    {
+        //좌우 캐릭터 회전
+        float yRotation = Input.GetAxisRaw("Mouse X");
+
+    }
+
+    private void cameraRotation()
+    {
+        //상하 카메라 회전
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+        float cameraRotationX = -xRotation * lookSensitivity;
+        currentCameraRotationX += cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        playerCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+
+    private void Move()
+    {
+        float moveDirX = Input.GetAxisRaw("Horizontal");
+        float moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * moveDirX;
+        Vector3 moveVertical = transform.forward * moveDirZ;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * walkSpeed;
+
+        myRigid.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+}
