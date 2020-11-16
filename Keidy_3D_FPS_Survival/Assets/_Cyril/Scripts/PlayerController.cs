165eba599a7bad5bbe4231b89d846ce7855eb697
Cyril Kim <jay.kim1994@gmail.com>
2020-11-16 17:42:14 +0900

    Added Running speed + change state Use mouse to rotate camera

--- Keidy_3D_FPS_Survival/Assets/_Cyril/Scripts/PlayerController.cs
+++ Keidy_3D_FPS_Survival/Assets/_Cyril/Scripts/PlayerController.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //스피드 조정 변수
     [SerializeField]
     private float walkSpeed;
+    [SerializeField]
+    private float runSpeed;
+    private float applySpeed;
 
+    //상태변수
+    private bool isRun = false;
+
+    //카메라 민감도
     [SerializeField]
     private float lookSensitivity;
 
+    //카메라 한계
     [SerializeField]
     private float cameraRotationLimit;
     private float currentCameraRotationX = 0f;
@@ -23,22 +32,52 @@ public class PlayerController : MonoBehaviour
     void Start()
     { 
         myRigid = GetComponent<Rigidbody>();
+        applySpeed = walkSpeed;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        tryRun();
         Move();
         cameraRotation();
         characterRotation();
+    }
+
+    private void tryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) 
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    private void Running()
+    {
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
     }
 
     private void characterRotation()
     {
         //좌우 캐릭터 회전
         float yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 characterRotationY = new Vector3(0f, yRotation, 0f) * lookSensitivity;
 
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(characterRotationY));
+        Debug.Log(myRigid.rotation);
+        Debug.Log(myRigid.rotation.eulerAngles);
     }
 
     private void cameraRotation()
@@ -60,7 +99,7 @@ public class PlayerController : MonoBehaviour
         Vector3 moveHorizontal = transform.right * moveDirX;
         Vector3 moveVertical = transform.forward * moveDirZ;
 
-        Vector3 velocity = (moveHorizontal + moveVertical).normalized * walkSpeed;
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * applySpeed;
 
         myRigid.MovePosition(transform.position + velocity * Time.deltaTime);
     }
